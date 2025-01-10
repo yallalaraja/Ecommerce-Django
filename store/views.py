41d783a0f253dfe8d33ecee2e7a4faa847e85c58
yallalarajareddy <yallalarajareddy1@gmail.com>
2025-01-10 16:47:39 +0530

    return the created order

--- store/views.py
+++ store/views.py
@@ -34,9 +34,13 @@ class ReviewViewSet(ModelViewSet):
 class OrderViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     
-    def get_serializer_context(self):
-        return {'user_id':self.request.user.id}
-        
+    def create(self, request, *args, **kwargs):
+        serializer = CreateOrderSerializer(data=request.data,context={'user_id':self.request.user.id})
+        serializer.is_valid(raise_exception=True)
+        order = serializer.save()
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)  
+
     def get_serializer_class(self):
         if self.request.method == 'POST':
             return CreateOrderSerializer
